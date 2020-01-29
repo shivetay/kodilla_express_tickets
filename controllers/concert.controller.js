7c9765753c5676370b7007ab9d37e3b0103cb66b
Łukasz Dawidowicz <lddev@outlook.com>
2020-01-29 10:53:55 +0100

    add concert conctroler change routes

---
+++ controllers/concert.controller.js
@@ -0,0 +1,69 @@
+const Concert = require('../models/concert.models');
+const uuidv1 = require('uuid/v1');
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Concert.find());
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getSIngle = async (req, res) => {
+  try {
+    const conc = await Concert.findById(req.params.id);
+    if (!conc) res.status(404).json({ message: 'There is no such concert' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.postSingle = async (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+  try {
+    const newConcert = new Concert({
+      performer: performer,
+      genre: genre,
+      price: price,
+      day: day,
+      image: image,
+    });
+    await newConcert.save();
+    res.jason({ messages: 'concert saved' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+exports.updateSingle = async (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+
+  try {
+    const conc = await Concert.findById(req.params.id);
+    if (conc) {
+      conc.performer = performer;
+      conc.genre = genre;
+      conc.price = price;
+      conc.day = day;
+      conc.image = image;
+      await conc.save();
+      res.json({ message: 'OK ' });
+    } else {
+      res.status(404).json({ message: 'Not found...' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.deleteSingle = async (req, res) => {
+  try {
+    const conc = await Concert.findById(req.params.id);
+    if (!conc) res.status(404).json({ message: 'Not Found' });
+    else {
+      await Concert.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- routes/concert.routes.js
+++ routes/concert.routes.js
@@ -1,59 +1,17 @@
 const express = require('express');
 const db = require('../db');
 const router = express.Router();
-const uuidv1 = require('uuid/v1');
 
-router.route('/concerts').get((req, res) => {
-  res.json(db.concerts);
-});
+const ConcertController = require('../controllers/concert.controller');
 
-router.route('/concerts/:id').get((req, res) => {
-  for (let post of db.concerts) {
-    if (req.params.id == post.id) {
-      return res.json(post);
-    }
-  }
-});
+router.get('/concerts', ConcertController.getAll);
 
-router.route('/concerts').post((req, res) => {
-  const { performer, genre, price, day, image } = req.body;
-  const newPost = {
-    performer: performer,
-    genre: genre,
-    price: price,
-    day: day,
-    image: image,
-    id: uuidv1(),
-  };
-  db.concerts.push(newPost);
-  return res.json({ message: 'OK' });
-});
+router.get('/concerts/:id', ConcertController.getSingle);
 
-router.route('/concerts/:id').put((req, res) => {
-  const { performer, genre, price, day, image } = req.body;
-  for (let post of db.concerts) {
-    if (
-      post.id == req.params.id &&
-      post.performer === performer &&
-      post.day === day
-    ) {
-      post.performer = performer;
-      post.genre = genre;
-      post.price = price;
-      post.day = day;
-      post.image = image;
-    }
-  }
-  res.json({ message: 'OK' });
-});
+router.post('/concerts', ConcertController.postSingle);
 
-router.route('/concerts/:id').delete((req, res) => {
-  for (let post of db.concerts) {
-    if (post.id === req.params.id) {
-      db.concerts.splice(db.concerts.indexOf(post), 1);
-    }
-  }
-  res.json({ message: 'OK' });
-});
+router.put('/concerts/:id', ConcertController.updateSingle);
+
+router.delete('/concerts/:id', ConcertController.deleteSingle);
 
 module.exports = router;
